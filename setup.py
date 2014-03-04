bfc74da217d5d804728690c558f71dba775fb367
Bradley <bradley@bradley-ThinkPad-X230.(none)>
2014-03-04 07:53:50 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(name = 'nbastats',
 	version = '1.0.1',
 	packages = ['nbastats'],
 	install_requires=['pandas>=0.13','requests>=2.2.1'],
+	author = 'Bradley Fay',
+	url = 'https://github.com/bradleyfay/nbadatacollection',
+	author_email = 'bradley.fay@gmail.com',
+	license = 'MIT'
 	)
