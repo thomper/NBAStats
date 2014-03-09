7929b943fc9cc39e1c0d22f81673e54deccfeae9
Bradley Fay <bradley.fay@gmail.com>
2014-03-09 07:46:07 -0700

    updated setup.py; repackaged version 1.1 and uploaded to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'nbastats',
-	version = '1.0.1',
+	version = '1.1.0',
 	packages = ['nbastats'],
 	install_requires=['pandas>=0.13','requests>=2.2.1'],
 	author = 'Bradley Fay',
